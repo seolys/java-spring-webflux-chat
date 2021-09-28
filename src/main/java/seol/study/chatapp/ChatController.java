38449d6478090cc8a4277ec4f79e3e937de23df8
설연수 <seolnavy@naver.com>
2021-09-28 14:22:14 +0900

    로그추가.

--- src/main/java/seol/study/chatapp/ChatController.java
+++ src/main/java/seol/study/chatapp/ChatController.java
@@ -1,7 +1,9 @@
 package seol.study.chatapp;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,14 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+@Slf4j
 @CrossOrigin
 @RestController // 데이터 리턴 서버
 @RequiredArgsConstructor
 public class ChatController {
 
 	private final ChatRepository chatRepository;
+	private final AtomicInteger atomicInteger = new AtomicInteger(1);
 
 	@GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
@@ -28,6 +32,7 @@ public class ChatController {
 
 	@GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+		log.info("{}. findByRoomNum={}", atomicInteger.getAndIncrement(), roomNum);
 		return chatRepository.mFindByRoomNum(roomNum)
 				.subscribeOn(Schedulers.boundedElastic());
 	}
